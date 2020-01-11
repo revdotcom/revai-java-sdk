2f3a40de30f99c8bb337cfff07d8e254975b891b
admin <admin@admin-4.local>
2020-01-10 20:06:49 -0800

    finish api client in a maven project

---
+++ src/test/java/revai/ApiRequestTest.java
@@ -0,0 +1,20 @@
+package revai;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ApiRequestTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
