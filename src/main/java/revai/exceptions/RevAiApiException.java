2c00dd58e3ad6d94b135c7da40a60b64aa54c410
chanc3 <chanceshiflett@yahoo.com>
2020-02-20 14:31:31 -0800

    Fix exception method to include error response info

--- src/main/java/revai/exceptions/RevAiApiException.java
+++ src/main/java/revai/exceptions/RevAiApiException.java
@@ -9,24 +9,42 @@ import java.io.IOException;
  * You can use this code to retrieve details of exceptions when calling the Rev.AI API.
  */
 public class RevAiApiException extends IOException {
-  public int statusCode;
-  public String title;
-  public String detail;
-  public String type;
-  public JSONObject errorResponse;
 
   public RevAiApiException(String message, JSONObject errorResponse, int responseCode) {
-    super(message);
-    this.errorResponse = errorResponse;
-    statusCode = responseCode;
+    super(formatErrorDetails(message, errorResponse, responseCode));
+  }
+
+  private static String formatErrorDetails(String message, JSONObject errorResponse, int responseCode) {
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append(message);
     if (errorResponse.has("title")) {
-      title = errorResponse.get("title").toString();
+      stringBuilder.append(System.lineSeparator());
+      stringBuilder.append("Title: " + errorResponse.get("title"));
     }
     if (errorResponse.has("detail")) {
-      detail = errorResponse.get("detail").toString();
+      stringBuilder.append(System.lineSeparator());
+      stringBuilder.append("Detail: " + errorResponse.get("detail"));
     }
     if (errorResponse.has("type")) {
-      type = errorResponse.get("type").toString();
+      stringBuilder.append(System.lineSeparator());
+      stringBuilder.append("Type: " + errorResponse.get("type"));
     }
+    if (errorResponse.has("status")) {
+      stringBuilder.append(System.lineSeparator());
+      stringBuilder.append("Status: " + errorResponse.get("status"));
+    }
+    if (errorResponse.has("parameters")) {
+      stringBuilder.append(System.lineSeparator());
+      stringBuilder.append("Parameters: " + errorResponse.get("parameters"));
+    }
+    if (errorResponse.has("allowed_values")) {
+      stringBuilder.append(System.lineSeparator());
+      stringBuilder.append("Allowed Values: " + errorResponse.get("allowed_values"));
+    }
+    if (errorResponse.has("current_value")) {
+      stringBuilder.append(System.lineSeparator());
+      stringBuilder.append("Current Value: " + errorResponse.get("current_value"));
+    }
+    return stringBuilder.toString();
   }
 }
