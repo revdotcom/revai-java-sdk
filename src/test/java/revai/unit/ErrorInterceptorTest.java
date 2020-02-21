f65c100babf14c82372a94814d382b872cc8bc59
chanc3 <chanceshiflett@yahoo.com>
2020-02-21 13:21:28 -0800

    Parameterize the error interceptor tests

--- src/test/java/revai/unit/ErrorInterceptorTest.java
+++ src/test/java/revai/unit/ErrorInterceptorTest.java
@@ -1,33 +1,46 @@
 package revai.unit;
 
 import okhttp3.Interceptor.Chain;
-import okhttp3.*;
-import org.assertj.core.api.Assertions;
-import org.junit.Assert;
+import okhttp3.MediaType;
+import okhttp3.Protocol;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.mockito.InjectMocks;
 import revai.ErrorInterceptor;
-import revai.exceptions.*;
+import revai.exceptions.RevAiApiException;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@RunWith(Parameterized.class)
 public class ErrorInterceptorTest {
   @InjectMocks private Chain mockChain;
 
-  // class to be tested
   private ErrorInterceptor sut;
-
   private Request sampleRequest;
   private Response sampleResponse;
 
+  private int statusCode;
+
+  public ErrorInterceptorTest(int statusCode) {
+    super();
+    this.statusCode = statusCode;
+  }
+
   @Before
-  public void setup() throws IOException {
+  public void setup() {
     sut = new ErrorInterceptor();
     mockChain = mock(Chain.class);
     sampleRequest = new Request.Builder().url("https://api.rev.ai/revspeech/v1/").build();
@@ -45,73 +58,26 @@ public class ErrorInterceptorTest {
             .build();
   }
 
+  @Parameterized.Parameters
+  public static Collection input() {
+    return Arrays.asList(new Object[][] {{200}, {400}, {401}, {404}, {406}, {409}, {429}, {500}});
+  }
+
   @Test
-  public void ErrorInterceptor_WhenResponseCodeIs400_ReturnsInvalidParameterException()
+  public void ErrorInterceptor_WhenResponseCodeIsGreaterThan399_ReturnsRevAiApiException()
       throws IOException {
-    when(mockChain.proceed(any(Request.class)))
-        .thenReturn(sampleResponse.newBuilder().code(400).build());
-    assertThatExceptionOfType(InvalidParameterException.class)
-        .isThrownBy(() -> sut.intercept(mockChain));
-  }
+    if (statusCode > 399) {
+      when(mockChain.proceed(any(Request.class)))
+              .thenReturn(sampleResponse.newBuilder().code(statusCode).build());
+      assertThatExceptionOfType(RevAiApiException.class)
+              .as("Expected status code [" + statusCode + "] to throw RevAiApiException")
+              .isThrownBy(() -> sut.intercept(mockChain));
+    } else {
+      when(mockChain.proceed(any(Request.class)))
+              .thenReturn(sampleResponse.newBuilder().code(statusCode).build());
+      Response response = sut.intercept(mockChain);
+      assertThat(response.code()).isEqualTo(statusCode);
+    }
 
-  @Test
-  public void ErrorInterceptor_WhenResponseCodeIs401_ReturnsAuthorizationException()
-      throws IOException {
-    when(mockChain.proceed(any(Request.class)))
-        .thenReturn(sampleResponse.newBuilder().code(401).build());
-    assertThatExceptionOfType(AuthorizationException.class)
-        .isThrownBy(() -> sut.intercept(mockChain));
-  }
-
-  @Test
-  public void ErrorInterceptor_WhenResponseCodeIs404_ReturnsResourceNotFoundException()
-      throws IOException {
-    when(mockChain.proceed(any(Request.class)))
-        .thenReturn(sampleResponse.newBuilder().code(404).build());
-    assertThatExceptionOfType(ResourceNotFoundException.class)
-        .isThrownBy(() -> sut.intercept(mockChain));
-  }
-
-  @Test
-  public void ErrorInterceptor_WhenResponseCodeIs406_ReturnsInvalidHeaderException()
-      throws IOException {
-    when(mockChain.proceed(any(Request.class)))
-        .thenReturn(sampleResponse.newBuilder().code(406).build());
-    assertThatExceptionOfType(InvalidHeaderException.class)
-        .isThrownBy(() -> sut.intercept(mockChain));
-  }
-
-  @Test
-  public void ErrorInterceptor_WhenResponseCodeIs409_ReturnsForbiddenStateException()
-      throws IOException {
-    when(mockChain.proceed(any(Request.class)))
-        .thenReturn(sampleResponse.newBuilder().code(409).build());
-    assertThatExceptionOfType(ForbiddenStateException.class)
-        .isThrownBy(() -> sut.intercept(mockChain));
-  }
-
-  @Test
-  public void ErrorInterceptor_WhenResponseCodeIs429_ReturnsThrottlingLimitException()
-      throws IOException {
-    when(mockChain.proceed(any(Request.class)))
-        .thenReturn(sampleResponse.newBuilder().code(429).build());
-    assertThatExceptionOfType(ThrottlingLimitException.class)
-        .isThrownBy(() -> sut.intercept(mockChain));
-  }
-
-  @Test
-  public void ErrorInterceptor_WhenResponseCodeIs500_ReturnsRevAiApiException() throws IOException {
-    int badResponseCode = 500;
-    when(mockChain.proceed(any(Request.class)))
-        .thenReturn(sampleResponse.newBuilder().code(badResponseCode).build());
-    assertThatExceptionOfType(RevAiApiException.class).isThrownBy(() -> sut.intercept(mockChain));
-  }
-
-  @Test
-  public void ErrorInterceptor_WhenResponseIs200_ReturnsOkHttpResponse() throws IOException {
-    when(mockChain.proceed(any(Request.class)))
-        .thenReturn(sampleResponse.newBuilder().code(200).build());
-    Response response = sut.intercept(mockChain);
-    assertThat(response.code()).isEqualTo(200);
   }
 }
