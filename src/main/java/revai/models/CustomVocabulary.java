a8dbff98941cc891d258d3e01b81f5ec701e721f
Chanc3 <chanceshiflett@yahoo.com>
2020-04-16 01:32:18 -0700

    Add javadoc to custom vocab and job options

--- src/main/java/revai/models/CustomVocabulary.java
+++ src/main/java/revai/models/CustomVocabulary.java
@@ -8,14 +8,31 @@ import java.util.List;
 public class CustomVocabulary {
   private List<String> phrases;
 
+  /**
+   * Creates a new custom vocabulary with a list of phrases limited to alphabetic characters and
+   * spaces. List size cannot exceed 20000 items.
+   *
+   * @param phrases A list of strings limited to alphabetic characters and spaces.
+   */
   public CustomVocabulary(List<String> phrases) {
     this.phrases = phrases;
   }
 
+  /**
+   * Returns a list of phrases
+   *
+   * @return A list of phrases
+   */
   public List<String> getPhrases() {
     return phrases;
   }
 
+  /**
+   * Sets the list of phrases to be used in the custom vocabulary. Phrases are limited to alphabetic
+   * characters and spaces. List size cannot exceed 20000 items.
+   *
+   * @param phrases The list of strings to set as the custom vocabulary phrases.
+   */
   public void setPhrases(List<String> phrases) {
     this.phrases = phrases;
   }
--- src/main/java/revai/models/asynchronous/RevAiJobOptions.java
+++ src/main/java/revai/models/asynchronous/RevAiJobOptions.java
@@ -5,7 +5,12 @@ import revai.models.CustomVocabulary;
 
 import java.util.List;
 
-/** A RevAiJobOptions object presents parameters that are submitted along a new job. */
+/**
+ * A RevAiJobOptions object represents parameters that are submitted along a new job.
+ *
+ * @see <a
+ *     href="https://www.rev.ai/docs#operation/SubmitTranscriptionJob">https://www.rev.ai/docs#operation/SubmitTranscriptionJob</a>
+ */
 public class RevAiJobOptions {
 
   /** The media url where the file can be downloaded. */
@@ -31,71 +36,144 @@ public class RevAiJobOptions {
   @SerializedName("speaker_channels_count")
   private Integer speakerChannelsCount;
 
-  /**
-   * Optional array of {@link CustomVocabulary} objects.
-   */
+  /** Optional array of {@link CustomVocabulary} objects. */
   @SerializedName("custom_vocabularies")
   private List<CustomVocabulary> customVocabularies;
 
-  /**
-   * Optional information that can be provided.
-   */
+  /** Optional information that can be provided. */
   @SerializedName("metadata")
   private String metadata;
 
-
+  /**
+   * Returns the media url.
+   *
+   * @return The media url.
+   */
   public String getMediaUrl() {
     return mediaUrl;
   }
 
+  /**
+   * Specifies the url where the media can be downloaded.
+   *
+   * @param mediaUrl The direct download url to the file.
+   */
   public void setMediaUrl(String mediaUrl) {
     this.mediaUrl = mediaUrl;
   }
 
+  /**
+   * Returns the callback url.
+   *
+   * @return the callback url.
+   */
   public String getCallbackUrl() {
     return callbackUrl;
   }
 
+  /**
+   * Specifies the callback url that Rev.ai will POST to when job processing is complete. This
+   * property is optional.
+   *
+   * @param callbackUrl The url to POST to when job processing is complete.
+   */
   public void setCallbackUrl(String callbackUrl) {
     this.callbackUrl = callbackUrl;
   }
 
+  /**
+   * Returns the value of the skip diarization Boolean.
+   *
+   * @return The skip diarization value.
+   */
   public Boolean getSkipDiarization() {
     return skipDiarization;
   }
 
+  /**
+   * Specifies if speaker diarization will be skipped by the speech engine. This property is
+   * optional and defaults to false.
+   *
+   * @param skipDiarization The value of the Boolean.
+   */
   public void setSkipDiarization(Boolean skipDiarization) {
     this.skipDiarization = skipDiarization;
   }
 
+  /**
+   * Returns the value of the skip punctuation Boolean.
+   *
+   * @return The skip punctuation value.
+   */
   public Boolean getSkipPunctuation() {
     return skipPunctuation;
   }
 
+  /**
+   * Specifies if the "punct" elements will be skipped by the speech engine. This property is
+   * optional and defaults to false.
+   *
+   * @param skipPunctuation The value of the Boolean.
+   */
   public void setSkipPunctuation(Boolean skipPunctuation) {
     this.skipPunctuation = skipPunctuation;
   }
 
+  /**
+   * Returns the speaker channel count.
+   *
+   * @return The speaker channel count.
+   */
   public Integer getSpeakerChannelsCount() {
     return speakerChannelsCount;
   }
 
+  /**
+   * Specifies the number of speaker channels in the audio. Each speaker channel is processed
+   * separately. When set the account will be charged the file * duration multiplied by the number
+   * of specified channels. This property is optional and defaults to null.
+   *
+   * @param speakerChannelsCount The number of separate speaker channels in the audio.
+   */
   public void setSpeakerChannelsCount(Integer speakerChannelsCount) {
     this.speakerChannelsCount = speakerChannelsCount;
   }
 
+  /**
+   * Returns a list of {@link CustomVocabulary} objects.
+   *
+   * @return A list of {@link CustomVocabulary} objects.
+   * @see CustomVocabulary
+   */
   public List<CustomVocabulary> getCustomVocabularies() {
     return customVocabularies;
   }
 
+  /**
+   * Provides the custom vocabularies to be used by the speech engine when processing the
+   * transcript. List size is limited to 50 items. Providing custom vocabularies is optional.
+   *
+   * @param customVocabularies A list of custom vocabularies.
+   * @see CustomVocabulary
+   */
   public void setCustomVocabularies(List<CustomVocabulary> customVocabularies) {
     this.customVocabularies = customVocabularies;
   }
 
+  /**
+   * Returns the metadata.
+   *
+   * @return A String that contains the metadata.
+   */
   public String getMetadata() {
     return metadata;
   }
 
+  /**
+   * Optional metadata that is provided during job submission limited to 512 characters.
+   *
+   * @param metadata A String to set as the metadata.
+   */
   public void setMetadata(String metadata) {
     this.metadata = metadata;
   }
