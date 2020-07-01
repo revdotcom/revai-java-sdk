87fe5371210fe633180f65e6eabcd5fc4974dd76
timitijani <53869176+timitijani@users.noreply.github.com>
2020-07-01 16:13:25 -0500

    AISDK-115: Add remove_disfluencies to Java sdk

--- src/test/java/ai/rev/speechtotext/unit/RevAiStreamingClientOptionalParametersTest.java
+++ src/test/java/ai/rev/speechtotext/unit/RevAiStreamingClientOptionalParametersTest.java
@@ -10,120 +10,125 @@ import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.experimental.theories.*;
+
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.mockito.Mockito;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(Parameterized.class)
+@RunWith(Theories.class)
 public class RevAiStreamingClientOptionalParametersTest {
 
-  private static final String VOCAB_ID = "VocabId";
-  private static final String METADATA = "Best Metadata";
-  private static final String FAKE_ACCESS_TOKEN = "foo";
+    private static final String VOCAB_ID = "VocabId";
+    private static final String METADATA = "Best Metadata";
+    private static final String FAKE_ACCESS_TOKEN = "foo";
 
-  private MockWebServer mockWebServer;
-  private StreamContentType defaultContentType;
-  private static StreamingClient streamingClient;
-  private SessionConfig sessionConfig = new SessionConfig();
+    private MockWebServer mockWebServer;
+    private StreamContentType defaultContentType;
+    private static StreamingClient streamingClient;
+    private SessionConfig sessionConfig = new SessionConfig();
 
-  public RevAiStreamingClientOptionalParametersTest(
-      String metadata, String customVocabularyId, Boolean filterProfanity) {
-    sessionConfig.setMetaData(metadata);
-    sessionConfig.setFilterProfanity(filterProfanity);
-    sessionConfig.setCustomVocabularyId(customVocabularyId);
-
-    this.defaultContentType = new StreamContentType();
-    defaultContentType.setContentType("audio/x-raw");
-    defaultContentType.setLayout("interleaved");
-    defaultContentType.setFormat("S16LE");
-    defaultContentType.setRate(16000);
-    defaultContentType.setChannels(1);
-  }
-
-  @Parameterized.Parameters
-  public static Collection input() {
-    return Arrays.asList(
-        new Object[][] {
-          {METADATA, VOCAB_ID, true},
-          {METADATA, VOCAB_ID, false},
-          {METADATA, null, true},
-          {METADATA, VOCAB_ID, null},
-          {null, VOCAB_ID, true},
-          {null, VOCAB_ID, false},
-          {null, VOCAB_ID, null},
-          {null, null, true},
-          {null, null, false},
-          {null, null, null}
-        });
-  }
-
-  @Before
-  public void setup() {
-    mockWebServer = new MockWebServer();
-    mockWebServer.enqueue(new MockResponse().withWebSocketUpgrade(new WebSocketListener() {}));
-    try {
-      mockWebServer.start();
-    } catch (IOException e) {
-      throw new RuntimeException("Unable to start Mock Web Server");
-    }
-    streamingClient = new StreamingClient(FAKE_ACCESS_TOKEN);
-    streamingClient.setScheme("http");
-    streamingClient.setHost(mockWebServer.getHostName());
-    streamingClient.setPort(mockWebServer.getPort());
-  }
-
-  @After
-  public void tearDown() {
-    try {
-      mockWebServer.close();
-    } catch (IOException e) {
-      throw new RuntimeException("Unable to close Mock Web Server");
-    }
-  }
-
-  @Test
-  public void StreamingClient_ParameterizedVariables_ContainsParametersInUrl()
-      throws UnsupportedEncodingException {
-    streamingClient.connect(
-        Mockito.mock(RevAiWebSocketListener.class), defaultContentType, sessionConfig);
-    RecordedRequest request;
-
-    try {
-      request = mockWebServer.takeRequest(1000, TimeUnit.MILLISECONDS);
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
+    @DataPoints("metadataAndNull")
+    public static String[] metadataAndNull() {
+        return new String[]{METADATA, null};
     }
 
-    if (request == null) {
-      throw new RuntimeException("No request was found");
+    @DataPoints("customVocabularyIdAndNull")
+    public static String[] vocabIdAndNull() {
+        return new String[]{VOCAB_ID, null};
     }
-    assertStreamOptions(request);
-  }
 
-  private void assertStreamOptions(RecordedRequest request) throws UnsupportedEncodingException {
-    if (sessionConfig.getFilterProfanity() != null) {
-      assertThat(request.getPath())
-          .contains("filter_profanity=" + sessionConfig.getFilterProfanity());
+    @DataPoints("booleanValuesAndNull")
+    public static Boolean[] booleanValuesAndNull() {
+        return new Boolean[]{true, false, null};
     }
-    if (sessionConfig.getCustomVocabularyId() != null) {
-      assertThat(request.getPath())
-          .contains("custom_vocabulary_id=" + sessionConfig.getCustomVocabularyId());
+
+    public RevAiStreamingClientOptionalParametersTest() {
+        this.defaultContentType = new StreamContentType();
+        defaultContentType.setContentType("audio/x-raw");
+        defaultContentType.setLayout("interleaved");
+        defaultContentType.setFormat("S16LE");
+        defaultContentType.setRate(16000);
+        defaultContentType.setChannels(1);
     }
-    if (sessionConfig.getMetaData() != null) {
-      assertThat(
-          URLDecoder.decode(request.getPath(), "UTF8")
-              .contains("metadata=" + sessionConfig.getMetaData()));
+
+    @Before
+    public void setup() {
+        mockWebServer = new MockWebServer();
+        mockWebServer.enqueue(new MockResponse().withWebSocketUpgrade(new WebSocketListener() {
+        }));
+        try {
+            mockWebServer.start();
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to start Mock Web Server");
+        }
+        streamingClient = new StreamingClient(FAKE_ACCESS_TOKEN);
+        streamingClient.setScheme("http");
+        streamingClient.setHost(mockWebServer.getHostName());
+        streamingClient.setPort(mockWebServer.getPort());
     }
-    assertThat(request.getPath()).contains("access_token=" + FAKE_ACCESS_TOKEN);
-  }
+
+    @After
+    public void tearDown() {
+        try {
+            mockWebServer.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to close Mock Web Server");
+        }
+    }
+
+    @Theory
+    public void StreamingClient_CustomVocabularyIdIsNull_ContainsParametersInUrl(
+            @FromDataPoints("metadataAndNull") String metadata,
+            @FromDataPoints("customVocabularyIdAndNull") String customVocabularyId,
+            @FromDataPoints("booleanValuesAndNull") Boolean filterProfanity,
+            @FromDataPoints("booleanValuesAndNull") Boolean removeDisfluencies
+    ) throws UnsupportedEncodingException {
+        sessionConfig.setMetaData(metadata);
+        sessionConfig.setFilterProfanity(filterProfanity);
+        sessionConfig.setCustomVocabularyId(customVocabularyId);
+        sessionConfig.setRemoveDisfluencies(removeDisfluencies);
+
+        streamingClient.connect(
+                Mockito.mock(RevAiWebSocketListener.class), defaultContentType, sessionConfig);
+        RecordedRequest request;
+
+        try {
+            request = mockWebServer.takeRequest(1000, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (request == null) {
+            throw new RuntimeException("No request was found");
+        }
+        assertStreamOptions(request);
+    }
+
+    private void assertStreamOptions(RecordedRequest request) throws UnsupportedEncodingException {
+        if (sessionConfig.getRemoveDisfluencies() != null) {
+            assertThat(request.getPath())
+                    .contains("remove_disfluencies=" + sessionConfig.getRemoveDisfluencies());
+        }
+        if (sessionConfig.getFilterProfanity() != null) {
+            assertThat(request.getPath())
+                    .contains("filter_profanity=" + sessionConfig.getFilterProfanity());
+        }
+        if (sessionConfig.getCustomVocabularyId() != null) {
+            assertThat(request.getPath())
+                    .contains("custom_vocabulary_id=" + sessionConfig.getCustomVocabularyId());
+        }
+        if (sessionConfig.getMetaData() != null) {
+            assertThat(
+                    URLDecoder.decode(request.getPath(), "UTF8")
+                            .contains("metadata=" + sessionConfig.getMetaData()));
+        }
+        assertThat(request.getPath()).contains("access_token=" + FAKE_ACCESS_TOKEN);
+    }
 }
