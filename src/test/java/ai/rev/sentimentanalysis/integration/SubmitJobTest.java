78ba3a5b68fcae0d46c4478686f14fb16ca4ea3d
kbridbur <kyle@rev.com>
2022-05-06 14:04:06 -0400

    Add tests

---
+++ src/test/java/ai/rev/sentimentanalysis/integration/SubmitJobTest.java
@@ -0,0 +1,88 @@
+package ai.rev.topicextraction.integration;
+
+import ai.rev.speechtotext.models.asynchronous.RevAiJobType;
+import ai.rev.speechtotext.models.asynchronous.RevAiTranscript;
+import ai.rev.testutils.EnvHelper;
+import ai.rev.sentimentanalysis.SentimentAnalysisClient;
+import ai.rev.sentimentanalysis.models.SentimentAnalysisJob;
+import ai.rev.sentimentanalysis.models.SentimentAnalysisJobOptions;
+import ai.rev.sentimentanalysis.models.SentimentAnalysisJobStatus;
+import com.google.gson.Gson;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import static ai.rev.testutils.FileHelper.getFileContents;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SubmitJobTest {
+
+  private final String TEXT_SOURCE =
+          getFileContents(Paths.get("./src/test/java/ai/rev/sentimentanalysis/resources/orange.txt"));
+  private final RevAiTranscript JSON_SOURCE =
+          (new Gson()).fromJson(
+                  getFileContents(Paths.get("./src/test/java/ai/rev/sentimentanalysis/resources/sample.json")),
+                  RevAiTranscript.class);
+  private static SentimentAnalysisClient apiClient;
+
+  @Rule public TestName testName = new TestName();
+
+  @Before
+  public void setup() {
+    apiClient = new SentimentAnalysisClient(EnvHelper.getToken());
+  }
+
+  @Test
+  public void SubmitJobText_TextAndOptionsSpecified_ReturnsJobInProgress()
+      throws IOException {
+    SentimentAnalysisJobOptions jobOptions = getJobOptions();
+
+    SentimentAnalysisJob job = apiClient.submitJobText(TEXT_SOURCE, jobOptions);
+
+    assertSentimentAnalysisJob(job);
+  }
+
+  @Test
+  public void SubmitJobText_OnlyTextIsSpecified_ReturnsJobInProgress()
+      throws IOException {
+    SentimentAnalysisJob job = apiClient.submitJobText(TEXT_SOURCE);
+
+    assertSentimentAnalysisJob(job);
+  }
+
+  @Test
+  public void SubmitJobJson_JsonAndOptionsSpecified_ReturnsJobInProgress()
+          throws IOException {
+    SentimentAnalysisJobOptions jobOptions = getJobOptions();
+
+    SentimentAnalysisJob job = apiClient.submitJobJson(JSON_SOURCE, jobOptions);
+
+    assertSentimentAnalysisJob(job);
+  }
+
+  @Test
+  public void SubmitJobLocalFile_OnlyJsonIsSpecified_ReturnsJobInProgress()
+      throws IOException {
+    SentimentAnalysisJob job = apiClient.submitJobJson(JSON_SOURCE);
+
+    assertSentimentAnalysisJob(job);
+  }
+
+  public void assertSentimentAnalysisJob(SentimentAnalysisJob job) {
+    assertThat(job.getJobId()).as("Job Id").isNotNull();
+    assertThat(job.getType()).as("Job Type").isEqualTo(RevAiJobType.TOPICEXTRACTION);
+    assertThat(job.getJobStatus()).as("Job status").isEqualTo(SentimentAnalysisJobStatus.IN_PROGRESS);
+  }
+
+  private SentimentAnalysisJobOptions getJobOptions() {
+    SentimentAnalysisJobOptions revAiJobOptions = new SentimentAnalysisJobOptions();
+    revAiJobOptions.setMetadata(testName.getMethodName());
+    revAiJobOptions.setCallbackUrl("https://example.com");
+    revAiJobOptions.setDeleteAfterSeconds(0);
+    return revAiJobOptions;
+  }
+}
--- src/test/java/ai/rev/sentimentanalysis/unit/SentimentAnalysisJobTest.java
+++ src/test/java/ai/rev/sentimentanalysis/unit/SentimentAnalysisJobTest.java
@@ -1,14 +1,14 @@
-package ai.rev.topicextraction.unit;
+package ai.rev.sentimentanalysis.unit;
 
 import ai.rev.helpers.MockInterceptor;
 import ai.rev.speechtotext.models.asynchronous.RevAiJobType;
 import ai.rev.speechtotext.models.asynchronous.RevAiTranscript;
 import ai.rev.testutils.AssertHelper;
-import ai.rev.topicextraction.TopicExtractionClient;
-import ai.rev.topicextraction.TopicExtractionInterface;
-import ai.rev.topicextraction.models.TopicExtractionJob;
-import ai.rev.topicextraction.models.TopicExtractionJobOptions;
-import ai.rev.topicextraction.models.TopicExtractionJobStatus;
+import ai.rev.sentimentanalysis.SentimentAnalysisClient;
+import ai.rev.sentimentanalysis.SentimentAnalysisInterface;
+import ai.rev.sentimentanalysis.models.SentimentAnalysisJob;
+import ai.rev.sentimentanalysis.models.SentimentAnalysisJobOptions;
+import ai.rev.sentimentanalysis.models.SentimentAnalysisJobStatus;
 import com.google.gson.Gson;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -25,55 +25,55 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-public class TopicExtractionJobTest {
+public class SentimentAnalysisJobTest {
   private OkHttpClient mockOkHttpClient;
   private MockInterceptor mockInterceptor;
-  private TopicExtractionClient sut;
+  private SentimentAnalysisClient sut;
 
   private final String JOB_ID = "testingID";
   private final String CREATED_ON = "2020-01-22T11:10:22.29Z";
   private final String COMPLETED_ON = "2020-01-22T11:13:22.29Z";
   private final MediaType MEDIA_TYPE = MediaType.get("application/json; charset=utf-8");
-  private final String JOBS_URL = "https://api.rev.ai/topic_extraction/v1/jobs";
+  private final String JOBS_URL = "https://api.rev.ai/sentiment_analysis/v1/jobs";
   private final String METADATA = "unit test";
   private Gson gson;
-  private TopicExtractionJob mockInProgressJob;
-  private TopicExtractionJob mockCompletedJob;
+  private SentimentAnalysisJob mockInProgressJob;
+  private SentimentAnalysisJob mockCompletedJob;
 
   @Before
   public void setup() {
     gson = new Gson();
 
-    mockInProgressJob = new TopicExtractionJob();
+    mockInProgressJob = new SentimentAnalysisJob();
     mockInProgressJob.setJobId(JOB_ID);
     mockInProgressJob.setCreatedOn(CREATED_ON);
-    mockInProgressJob.setJobStatus(TopicExtractionJobStatus.IN_PROGRESS);
+    mockInProgressJob.setJobStatus(SentimentAnalysisJobStatus.IN_PROGRESS);
     mockInProgressJob.setType(RevAiJobType.TOPICEXTRACTION);
     mockInProgressJob.setMetadata(METADATA);
 
     mockCompletedJob = mockInProgressJob;
-    mockCompletedJob.setJobStatus(TopicExtractionJobStatus.COMPLETED);
+    mockCompletedJob.setJobStatus(SentimentAnalysisJobStatus.COMPLETED);
     mockCompletedJob.setCompletedOn(COMPLETED_ON);
 
-    sut = new TopicExtractionClient("validToken");
+    sut = new SentimentAnalysisClient("validToken");
     mockInterceptor = new MockInterceptor(MEDIA_TYPE, 200);
     mockOkHttpClient = new OkHttpClient.Builder().addInterceptor(mockInterceptor).build();
     Retrofit mockRetrofit =
         new Retrofit.Builder()
-            .baseUrl("https://api.rev.ai/topic_extraction/v1/")
+            .baseUrl("https://api.rev.ai/sentiment_analysis/v1/")
             .addConverterFactory(ScalarsConverterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .client(mockOkHttpClient)
             .build();
 
-    sut.apiInterface = mockRetrofit.create(TopicExtractionInterface.class);
+    sut.apiInterface = mockRetrofit.create(SentimentAnalysisInterface.class);
   }
 
   @Test
   public void GetJobDetails_JobIdIsValid_ReturnsRevAiJob() throws IOException {
     mockInterceptor.setSampleResponse(gson.toJson(mockInProgressJob));
 
-    TopicExtractionJob job = sut.getJobDetails(JOB_ID);
+    SentimentAnalysisJob job = sut.getJobDetails(JOB_ID);
 
     String expectedUrl = JOBS_URL + "/" + JOB_ID;
     AssertHelper.assertRequestMethodAndUrl(mockInterceptor, "GET", expectedUrl);
@@ -82,12 +82,12 @@ public class TopicExtractionJobTest {
 
   @Test
   public void GetListOfJobs_NoArguments_ReturnsAListOfRevAiJobs() throws IOException {
-    List<TopicExtractionJob> mockJobList = new ArrayList<>();
+    List<SentimentAnalysisJob> mockJobList = new ArrayList<>();
     mockJobList.add(mockInProgressJob);
     mockJobList.add(mockCompletedJob);
     mockInterceptor.setSampleResponse(gson.toJson(mockJobList));
 
-    List<TopicExtractionJob> jobs = sut.getListOfJobs();
+    List<SentimentAnalysisJob> jobs = sut.getListOfJobs();
 
     AssertHelper.assertRequestMethodAndUrl(mockInterceptor, "GET", JOBS_URL);
     assertThat(jobs.size()).isEqualTo(mockJobList.size());
@@ -97,11 +97,11 @@ public class TopicExtractionJobTest {
   @Test
   public void GetListOfJobs_JobLimitIsOne_ReturnsARevAiJobListSizeOfOne() throws IOException {
     Integer SAMPLE_LIMIT = 1;
-    List<TopicExtractionJob> mockJobList = new ArrayList<>();
+    List<SentimentAnalysisJob> mockJobList = new ArrayList<>();
     mockJobList.add(mockCompletedJob);
     mockInterceptor.setSampleResponse(gson.toJson(mockJobList));
 
-    List<TopicExtractionJob> jobs = sut.getListOfJobs(SAMPLE_LIMIT);
+    List<SentimentAnalysisJob> jobs = sut.getListOfJobs(SAMPLE_LIMIT);
 
     String expectedUrl = JOBS_URL + "?limit=" + SAMPLE_LIMIT;
     AssertHelper.assertRequestMethodAndUrl(mockInterceptor, "GET", expectedUrl);
@@ -112,12 +112,12 @@ public class TopicExtractionJobTest {
   @Test
   public void GetListOfJobs_StartAfterIsSpecified_ReturnsAListOfRevAiJobs() throws IOException {
     String sampleID = "sampleID";
-    List<TopicExtractionJob> mockJobList = new ArrayList<>();
+    List<SentimentAnalysisJob> mockJobList = new ArrayList<>();
     mockJobList.add(mockInProgressJob);
     mockJobList.add(mockCompletedJob);
     mockInterceptor.setSampleResponse(gson.toJson(mockJobList));
 
-    List<TopicExtractionJob> revAiJobs = sut.getListOfJobs(sampleID);
+    List<SentimentAnalysisJob> revAiJobs = sut.getListOfJobs(sampleID);
 
     String expectedUrl = JOBS_URL + "?starting_after=" + sampleID;
     AssertHelper.assertRequestMethodAndUrl(mockInterceptor, "GET", expectedUrl);
@@ -129,11 +129,11 @@ public class TopicExtractionJobTest {
     String SAMPLE_TEXT = "sample";
     mockInterceptor.setSampleResponse(gson.toJson(mockInProgressJob));
 
-    TopicExtractionJob revAiJob = sut.submitJobText(SAMPLE_TEXT, null);
+    SentimentAnalysisJob revAiJob = sut.submitJobText(SAMPLE_TEXT, null);
 
-    TopicExtractionJobOptions options = new TopicExtractionJobOptions();
+    SentimentAnalysisJobOptions options = new SentimentAnalysisJobOptions();
     options.setText(SAMPLE_TEXT);
-    AssertHelper.assertRequestBody(mockInterceptor, options, TopicExtractionJobOptions.class);
+    AssertHelper.assertRequestBody(mockInterceptor, options, SentimentAnalysisJobOptions.class);
     AssertHelper.assertRequestMethodAndUrl(mockInterceptor, "POST", JOBS_URL);
     assertRevAiJob(revAiJob, mockInProgressJob);
   }
@@ -142,14 +142,14 @@ public class TopicExtractionJobTest {
   public void SubmitJobText_TextAndOptionsAreSpecified_ReturnsARevAiJob() throws IOException {
     String SAMPLE_TEXT = "sample";
     mockInterceptor.setSampleResponse(gson.toJson(mockInProgressJob));
-    TopicExtractionJobOptions options = new TopicExtractionJobOptions();
+    SentimentAnalysisJobOptions options = new SentimentAnalysisJobOptions();
     options.setCallbackUrl("https://example.com");
     options.setMetadata(METADATA);
     options.setDeleteAfterSeconds(0);
 
-    TopicExtractionJob revAiJob = sut.submitJobText(SAMPLE_TEXT, options);
+    SentimentAnalysisJob revAiJob = sut.submitJobText(SAMPLE_TEXT, options);
 
-    AssertHelper.assertRequestBody(mockInterceptor, options, TopicExtractionJobOptions.class);
+    AssertHelper.assertRequestBody(mockInterceptor, options, SentimentAnalysisJobOptions.class);
     AssertHelper.assertRequestMethodAndUrl(mockInterceptor, "POST", JOBS_URL);
     assertRevAiJob(revAiJob, mockInProgressJob);
   }
@@ -165,11 +165,11 @@ public class TopicExtractionJobTest {
     RevAiTranscript SAMPLE_JSON = new RevAiTranscript();
     mockInterceptor.setSampleResponse(gson.toJson(mockInProgressJob));
 
-    TopicExtractionJob revAiJob = sut.submitJobJson(SAMPLE_JSON);
+    SentimentAnalysisJob revAiJob = sut.submitJobJson(SAMPLE_JSON);
 
-    TopicExtractionJobOptions options = new TopicExtractionJobOptions();
+    SentimentAnalysisJobOptions options = new SentimentAnalysisJobOptions();
     options.setJson(SAMPLE_JSON);
-    AssertHelper.assertRequestBody(mockInterceptor, options, TopicExtractionJobOptions.class);
+    AssertHelper.assertRequestBody(mockInterceptor, options, SentimentAnalysisJobOptions.class);
     AssertHelper.assertRequestMethodAndUrl(mockInterceptor, "POST", JOBS_URL);
     assertRevAiJob(revAiJob, mockInProgressJob);
   }
@@ -179,11 +179,11 @@ public class TopicExtractionJobTest {
       throws IOException {
     RevAiTranscript SAMPLE_JSON = new RevAiTranscript();
     mockInterceptor.setSampleResponse(gson.toJson(mockInProgressJob));
-    TopicExtractionJobOptions options = new TopicExtractionJobOptions();
+    SentimentAnalysisJobOptions options = new SentimentAnalysisJobOptions();
 
-    TopicExtractionJob revAiJob = sut.submitJobJson(SAMPLE_JSON, options);
+    SentimentAnalysisJob revAiJob = sut.submitJobJson(SAMPLE_JSON, options);
 
-    AssertHelper.assertRequestBody(mockInterceptor, options, TopicExtractionJobOptions.class);
+    AssertHelper.assertRequestBody(mockInterceptor, options, SentimentAnalysisJobOptions.class);
     AssertHelper.assertRequestMethodAndUrl(mockInterceptor, "POST", JOBS_URL);
     assertRevAiJob(revAiJob, mockInProgressJob);
   }
@@ -205,10 +205,10 @@ public class TopicExtractionJobTest {
     AssertHelper.assertRequestMethodAndUrl(mockInterceptor, "DELETE", expectedUrl);
   }
 
-  private void assertJobsList(List<TopicExtractionJob> jobs) {
+  private void assertJobsList(List<SentimentAnalysisJob> jobs) {
     jobs.forEach(
         job -> {
-          if (job.getJobStatus().equals(TopicExtractionJobStatus.COMPLETED)) {
+          if (job.getJobStatus().equals(SentimentAnalysisJobStatus.COMPLETED)) {
             assertThat(gson.toJson(job)).isEqualTo(gson.toJson(mockCompletedJob));
           } else {
             assertThat(gson.toJson(job)).isEqualTo(gson.toJson(mockInProgressJob));
@@ -216,7 +216,7 @@ public class TopicExtractionJobTest {
         });
   }
 
-  private void assertRevAiJob(TopicExtractionJob actualJob, TopicExtractionJob expectedJob) {
-    assertThat(gson.toJson(actualJob)).as("TopicExtractionJob").isEqualTo(gson.toJson(expectedJob));
+  private void assertRevAiJob(SentimentAnalysisJob actualJob, SentimentAnalysisJob expectedJob) {
+    assertThat(gson.toJson(actualJob)).as("SentimentAnalysisJob").isEqualTo(gson.toJson(expectedJob));
   }
 }
